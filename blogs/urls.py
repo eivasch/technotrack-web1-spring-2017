5888ac910625f4d4b078e46bcbd23290c7589867
elena <elenaiv100@gmail.com>
2017-03-13 18:01:16 +0300

    change render to DetailView

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from .views import BlogList, show_blog, show_post, PostList
+from .views import BlogList, BlogDetail, PostList, PostDetail
 
 urlpatterns = [
     url(r'^$', BlogList.as_view(), name="blog_list"),
     url(r'^(?P<blog_id>\d+)/posts/$', PostList.as_view(), name="post_list"),
-    url(r'^(?P<blog_id>\d+)$', show_blog, name="one_blog"),
-    url(r'^(?P<blog_id>\d+)/posts/(?P<post_id>\d+)$', show_post, name="one_post"),
+    url(r'^(?P<pk>\d+)$', BlogDetail.as_view(), name="one_blog"),
+    url(r'^(?P<blog_id>\d+)/posts/(?P<pk>\d+)$', PostDetail.as_view(), name="one_post"),
 ]
